dd95181953d1a4eeddb39a010e46fbf1ae969f4e
jasonhong94 <43994669+jasonhong94@users.noreply.github.com>
2020-06-19 22:27:22 +0800

    Add files via upload

---
+++ Milestone 5/Mobile App development/app/src/main/java/com/example/cjshcryptoapp/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.cjshcryptoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView myWebView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myWebView = (WebView)findViewById(R.id.webView);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.loadUrl("https://cryptofortwp.herokuapp.com/");
+        myWebView.setWebViewClient(new WebViewClient());
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(myWebView.canGoBack()) {
+            myWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
